fe09790fbe091c54b0bc1cf146ac978faf64a809
Mavlon <Mavlon90@users.noreply.github.com>
2020-02-02 14:05:54 +0500

    Some changed in Tel..

--- src/components/Telegram.js
+++ src/components/Telegram.js
@@ -4,8 +4,8 @@ import TelegramItem from './TelegramItem'
 export default function Telegram() {
     const items = [
         {id: 1, img: "https://i.pravatar.cc/150?img=9", important: true, name: 'Anna Karenina', msg:'Привет!,', tags:['#friend', '#funny'], status: true},
-        {id: 2, img: "https://i.pravatar.cc/150?img=10", important: false, name: 'Tanya Volchuk', msg:'Hello, every body.', tags:['#friend', '#funny'], status:false},
-        {id: 2, img: "https://i.pravatar.cc/150?img=11", important: false, name: 'Pavel Shapkin', msg:'Салют всем!,', tags:['#friend', '#funny'], status:true},
+        {id: 2, img: "https://i.pravatar.cc/150?img=10", important: true, name: 'Tanya Volchuk', msg:'Hello, every body.', tags:['#friend', '#funny'], status:true},
+        {id: 2, img: "https://i.pravatar.cc/150?img=11", important: true, name: 'Pavel Shapkin', msg:'Салют всем!,', tags:['#friend', '#funny'], status:false},
     ]
     return (
         <div className="important">  

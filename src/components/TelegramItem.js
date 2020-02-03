3c61f113493537c58e2d0be2b5d3397466694b23
Mavlon <Mavlon90@users.noreply.github.com>
2020-02-03 21:23:16 +0500

    Some changed

--- src/components/TelegramItem.js
+++ src/components/TelegramItem.js
@@ -8,7 +8,7 @@ export default function TelegramItem(props) {
         <>
         <div className={props.important === true ? "imporant" : undefined}>
             <img src={props.img} alt="" />
-            <span className={(props.status == true) ? "liked" : undefined}>{props.status}☑</span>
+            <span className={(props.status === true) ? "liked" : undefined}>{props.status}☑</span>
             <div id='name'><h4>{props.name}</h4> </div>
             <div id='msg'><h5>{props.msg}</h5></div>
             {tags}

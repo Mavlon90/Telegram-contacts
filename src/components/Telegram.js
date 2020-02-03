ec4255618747225827b7d9929d05387c4d97b09d
Mavlon <Mavlon90@users.noreply.github.com>
2020-02-03 21:11:12 +0500

    Project reloaded & pushed

--- src/components/Telegram.js
+++ src/components/Telegram.js
@@ -5,7 +5,7 @@ export default function Telegram() {
     const items = [
         {id: 1, img: "https://i.pravatar.cc/150?img=9", important: true, name: 'Anna Karenina', msg:'Привет!,', tags:['#friend', '#funny'], status: true},
         {id: 2, img: "https://i.pravatar.cc/150?img=10", important: true, name: 'Tanya Volchuk', msg:'Hello, every body.', tags:['#friend', '#funny'], status:true},
-        {id: 2, img: "https://i.pravatar.cc/150?img=11", important: true, name: 'Pavel Shapkin', msg:'Салют всем!,', tags:['#friend', '#funny'], status:false},
+        {id: 3, img: "https://i.pravatar.cc/150?img=11", important: true, name: 'Pavel Shapkin', msg:'Салют всем!,', tags:['#friend', '#funny'], status:false},
     ]
     return (
         <div className="important">  

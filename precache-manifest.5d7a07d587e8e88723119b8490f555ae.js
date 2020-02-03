a14d5fbeaf16ec747c0acc85693faea1ad764ad4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-03 16:25:00 +0000

    Deploy Mavlon90/Telegram-contacts to github.com/Mavlon90/Telegram-contacts.git:gh-pages

---
+++ precache-manifest.5d7a07d587e8e88723119b8490f555ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5dbfc672bee202204883876081d757",
+    "url": "/Telegram-contacts/index.html"
+  },
+  {
+    "revision": "2680d0d1584c28a9145e",
+    "url": "/Telegram-contacts/static/css/main.bb6a7da2.chunk.css"
+  },
+  {
+    "revision": "5563255d733454464442",
+    "url": "/Telegram-contacts/static/js/2.9c10cef0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Telegram-contacts/static/js/2.9c10cef0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2680d0d1584c28a9145e",
+    "url": "/Telegram-contacts/static/js/main.da38a0f0.chunk.js"
+  },
+  {
+    "revision": "463daf28a4b93cbd7ed8",
+    "url": "/Telegram-contacts/static/js/runtime-main.4332e4af.js"
+  }
+]);
